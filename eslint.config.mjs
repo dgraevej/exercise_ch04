408627fd3ff21ec9cb229f7509c85dc2225fa29f
dgraevej <johan.de.graeve@brusselsairport.be>
2024-07-12 08:29:16 +0200

    -

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import { fixupConfigRules } from "@eslint/compat";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...fixupConfigRules(compat.extends("plugin:react/recommended", "plugin:react/jsx-runtime")),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+
+            ecmaVersion: "latest",
+            sourceType: "module",
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            semi: [2, "always"],
+        },
+    },
+];
